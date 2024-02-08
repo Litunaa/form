55910bc99eac4ae6996170878d5b25799470d9af
litunaa <nubtem1337@gmail.com>
2024-02-08 17:30:04 +0100

    fixed script

--- script.js
+++ script.js
@@ -21,47 +21,44 @@ function validateForm(){
 function validateSocialSecurityNumber(){
     let SSN = document.getElementById("SSN").value;
 
-    if (!SSN){
-        return "empty";
-    }
 
-
-    // "Year" and "Personal Number" isn't necessary to check since they are valid for all numbers. 
-    let month = parseInt(SSN.substring(2, 4));
-    let day = parseInt(SSN.substring(4, 6));
+    if (SSN){
+        // "Year" and "Personal Number" isn't necessary to check since they are valid for all numbers. 
+        let month = parseInt(SSN.substring(2, 4));
+        let day = parseInt(SSN.substring(4, 6));
 
 
 
-    if (!(month >= 1 && month <= 12)){
-        alert("Month out of range");
-        return false;
-    }
-
-    
-
-    if (month === 2){
-        if (!(day >= 1 && day <= 28)){
-            alert("Day out of range");
+        if (!(month >= 1 && month <= 12)){
+            alert("Month out of range");
             return false;
         }
-    }
 
-    let evenMonth = month % 2 === 0;
+        
 
-    if (evenMonth){
-        if (!(day >= 1 && day <= 30)){
-            alert("Day out of range");
-            return false;
+        if (month === 2){
+            if (!(day >= 1 && day <= 28)){
+                alert("Day out of range");
+                return false;
+            }
+        }
+
+        let evenMonth = month % 2 === 0;
+
+        if (evenMonth){
+            if (!(day >= 1 && day <= 30)){
+                alert("Day out of range");
+                return false;
+            }
+        }
+
+        else{
+            if (!(day >= 1 && day <= 31)){
+                alert("Day out of range");
+                return false;
+            }
         }
     }
-
-    else{
-        if (!(day >= 1 && day <= 31)){
-            alert("Day out of range");
-            return false;
-        }
-    }
-
     return true;
 }
 
